ececce70bec1420ea071d05d94ff7adef4a4cae1
Dotssy <zozulya.bohdan@gmail.com>
2019-06-20 15:59:04 +0400

    commit

---
+++ src/components/Article.jsx
@@ -0,0 +1,58 @@
+import React, {PureComponent} from 'react'
+
+class Article extends PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      
+    }
+  }
+
+  /*
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.state.isOpen !== nextState.isOpen
+  }
+  */
+
+  componentWillMount() {
+    
+  }
+
+  /*
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
+      isOpen: nextProps.defaultOpen
+    })
+  }
+
+  componentWillUpdate() {
+
+  }
+  */
+
+  render() {
+    const {article, isOpen, onButtonClick} = this.props
+    const body = isOpen && <section className="card-text">{article.text}</section>;
+    return (
+        <div className='card mx-auto' style = {{width: '50%'}}>
+            <div className="card-header">
+              <h2>
+                {article.title}
+                <button onClick={onButtonClick} className="btn btn-primary btn-lg float-right">
+                  {isOpen ? 'close' : 'open'}
+                </button>
+              </h2>
+            </div>
+            <div className="card-body">
+              <h6 className="card-subtitle text-muted">
+                creation date: {(new Date(article.date)).toDateString()}
+              </h6>
+              {body}
+            </div>
+        </div>
+      )
+  }
+}
+
+export default Article
---
+++ src/components/ArticleList/index.jsx
@@ -0,0 +1,28 @@
+import React, {PureComponent} from 'react'
+import Article from '../Article'
+import './style.css'
+
+export default class ArticleList extends PureComponent {
+	state = {
+		openArticleId: null
+	}
+	render() {
+		const articleElements = this.props.articles.map((article, index) => 
+			<li className="article-list__li" key = {article.id} >
+				<Article article = {article} 
+					isOpen = {this.state.openArticleId === article.id} 
+					onButtonClick = {this.handleClick.bind(this, article.id)}
+				/>
+			</li>
+		)
+		return (
+				<ul className="article-list__ul">
+					{articleElements}
+				</ul>
+		)
+	}
+
+	handleClick = openArticleId => this.setState({ 
+		openArticleId: this.state.openArticleId === openArticleId ? null : openArticleId
+	})
+}
